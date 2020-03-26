df009f8267ab2cdbb8e20eeee30233a51ff8cf92
Chris Campbell <cacampbell@ucdavis.edu>
2020-03-26 16:36:18 -0700

    Layout test

--- src/test/java/llc/launchbadge/LogstashOutputHcsTest.java
+++ src/test/java/llc/launchbadge/LogstashOutputHcsTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.logstash.plugins.ConfigurationImpl;
 import llc.launchbadge.LogstashOutputHcs;
 
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -17,33 +16,16 @@ public class LogstashOutputHcsTest {
 
     @Test
     public void testLogstashOutputHcs() {
-        String prefix = "Prefix";
         Map<String, Object> configValues = new HashMap<>();
-        configValues.put(LogstashOutputHcs.PREFIX_CONFIG.name(), prefix);
-        Configuration config = new ConfigurationImpl(configValues);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        LogstashOutputHcs output = new LogstashOutputHcs("test-id", config, null, baos);
+        // Fill in configuration options
 
-        String sourceField = "message";
-        int eventCount = 5;
+        Configuration config = new ConfigurationImpl(configValues);
+        LogstashOutputHcs output = new LogstashOutputHcs("test-id", config, null);
+
         Collection<Event> events = new ArrayList<>();
-        for (int k = 0; k < eventCount; k++) {
-            Event e = new org.logstash.Event();
-            e.setField(sourceField, "message " + k);
-            events.add(e);
-        }
+        // Add events here
 
         output.output(events);
-
-        String outputString = baos.toString();
-        int index = 0;
-        int lastIndex = 0;
-        while (index < eventCount) {
-            lastIndex = outputString.indexOf(prefix, lastIndex);
-            Assert.assertTrue("Prefix should exist in output string", lastIndex > -1);
-            lastIndex = outputString.indexOf("message " + index);
-            Assert.assertTrue("Message should exist in output string", lastIndex > -1);
-            index++;
-        }
+        // Subscribe to HCS with parameters from above and check that events were received
     }
 }
